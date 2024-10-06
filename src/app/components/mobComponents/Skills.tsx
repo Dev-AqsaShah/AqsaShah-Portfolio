422f7ce7e02efcd062fdd5d77316fca402f025a8
aqsashah <159179853+Dev-AqsaShah@users.noreply.github.com>
2024-10-06 17:41:49 +0500

    Update Skills.tsx

--- src/app/components/mobComponents/Skills.tsx
+++ src/app/components/mobComponents/Skills.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react"
 import { FaBootstrap,  FaCss3Alt, FaDocker, FaFigma, FaGithub, FaHtml5, FaLinux, FaNpm, FaPython, FaReact } from "react-icons/fa";
 import { SiGooglecolab, SiJupyter, SiShadcnui, SiTypescript } from 'react-icons/si';
 import { RiNextjsFill, RiTailwindCssFill } from 'react-icons/ri';
@@ -16,16 +16,16 @@ function MobileSkills() {
 <div className="mt-8 p-4 w-full md:w-1/3 ">
    <div className="flex items-center mb-3">
      <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-white text-white flex-shrink-0">
-   < FaHtml5 className='text-blue-900 text-lg'/>
+   < FaHtml5 className="text-blue-900 bg-white text-lg"/>
      </div>
      <h2 className="text-white text-lg font-semibold tracking-tight">
        HTML
        </h2>
    </div>
-     <div className="w-full h-1 rounded bg-black">
+     <div className="w-full h-1 rounded bg-white">
        <div className='w-[95%] h-1 bg-white rounded-lg'/>
      </div>
-     <h3 className='text-white text-sm font-semibold text-right tracking-tight'>98%</h3>
+     <h3 className="text-white text-sm font-semibold text-right tracking-tight">98%</h3>
  </div>
 
 
@@ -33,13 +33,13 @@ function MobileSkills() {
 <div className="p-4 w-full md:w-1/3 ">
    <div className="flex items-center mb-3">
      <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-white text-white flex-shrink-0">
-   < FaCss3Alt className='text-blue-900 text-lg'/>
+   < FaCss3Alt className='text-blue-900 bg-white text-lg'/>
      </div>
      <h2 className="text-white text-lg font-semibold tracking-tight">
        CSS
        </h2>
    </div>
-     <div className="w-full h-1 rounded bg-black">
+     <div className="w-full h-1 rounded bg-white">
        <div className='w-[94%] h-1 bg-white rounded-lg'/>
      </div>
      <h3 className='text-white text-sm font-semibold text-right tracking-tight'>96%</h3>
@@ -50,14 +50,14 @@ function MobileSkills() {
  <div className="p-4 w-full md:w-1/3 ">
    <div className="flex items-center mb-3">
      <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-white text-white flex-shrink-0">
-   < SiTypescript className='text-blue-900 text-lg'/>
+   < SiTypescript className="text-blue-900 text-lg"/>
      </div>
      <h2 className="text-white text-lg font-semibold tracking-tight">
        TYPESCRIPT
        </h2>
    </div>
      <div className="w-full h-1 rounded bg-black">
-       <div className='w-[92%] h-1 bg-white rounded-lg'/>
+       <div className="w-[92%] h-1 bg-white rounded-lg"/>
      </div>
      <h3 className='text-white text-sm font-semibold text-right tracking-tight'>93%</h3>
  </div>
@@ -67,16 +67,16 @@ function MobileSkills() {
  <div className="p-4 w-full md:w-1/3 ">
    <div className="flex items-center mb-3">
      <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-white text-white flex-shrink-0">
-   < FaReact className='text-blue-900 text-2xl'/>
+   < FaReact className="text-blue-900 text-2xl"/>
      </div>
      <h2 className="text-white text-lg font-semibold tracking-tight">
        REACT
        </h2>
    </div>
      <div className="w-full h-1 rounded bg-black">
-       <div className='w-[85%] h-1 bg-white rounded-lg'/>
+       <div className="w-[85%] h-1 bg-white rounded-lg"/>
      </div>
-     <h3 className='text-white text-sm font-semibold text-right tracking-tight'>85%</h3>
+     <h3 className="text-white text-sm font-semibold text-right tracking-tight">85%</h3>
  </div>
 
 
@@ -84,16 +84,16 @@ function MobileSkills() {
  <div className="p-4 w-full md:w-1/3 ">
    <div className="flex items-center mb-3">
      <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-white text-white flex-shrink-0">
-   < RiNextjsFill className='text-blue-900 text-2xl'/>
+   < RiNextjsFill className="text-blue-900 text-2xl"/>
      </div>
      <h2 className="text-white text-lg font-semibold tracking-tight">
        NEXT.JS
        </h2>
    </div>
      <div className="w-full h-1 rounded bg-black">
-       <div className='w-[90%] h-1 bg-white rounded-lg'/>
+       <div className="w-[90%] h-1 bg-white rounded-lg"/>
      </div>
-     <h3 className='text-white text-sm font-semibold text-right tracking-tight'>90%</h3>
+     <h3 className="text-white text-sm font-semibold text-right tracking-tight">90%</h3>
  </div>
 
 
@@ -101,16 +101,16 @@ function MobileSkills() {
   <div className="p-4 w-full md:w-1/3 ">
    <div className="flex items-center mb-3">
      <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-white text-white flex-shrink-0">
-   < RiNextjsFill className='text-blue-900 text-2xl'/>
+   < RiNextjsFill className="text-blue-900 text-2xl"/>
      </div>
      <h2 className="text-white text-lg font-semibold tracking-tight">
        NODE.JS
        </h2>
    </div>
      <div className="w-full h-1 rounded bg-black">
-       <div className='w-[90%] h-1 bg-white rounded-lg'/>
+       <div className="w-[90%] h-1 bg-white rounded-lg"/>
      </div>
-     <h3 className='text-white text-sm font-semibold text-right tracking-tight'>90%</h3>
+     <h3 className="text-white text-sm font-semibold text-right tracking-tight">90%</h3>
  </div>
 
 
@@ -118,16 +118,16 @@ function MobileSkills() {
  <div className="p-4 w-full md:w-1/3 ">
    <div className="flex items-center mb-3">
      <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-white text-white flex-shrink-0">
-   < RiTailwindCssFill className='text-blue-900 text-2xl'/>
+   < RiTailwindCssFill className="text-blue-900 text-2xl"/>
      </div>
      <h2 className="text-white text-lg font-semibold tracking-tight">
        TAILWIND
        </h2>
    </div>
      <div className="w-full h-1 rounded bg-black">
-       <div className='w-[90%] h-1 bg-white rounded-lg'/>
+       <div className="w-[90%] h-1 bg-white rounded-lg"/>
      </div>
-     <h3 className='text-white text-sm font-semibold text-right tracking-tight'>92%</h3>
+     <h3 className="text-white text-sm font-semibold text-right tracking-tight">92%</h3>
  </div>
 
 
@@ -135,16 +135,16 @@ function MobileSkills() {
  <div className="p-4 w-full md:w-1/3 ">
    <div className="flex items-center mb-3">
      <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-white text-white flex-shrink-0">
-   < RiTailwindCssFill className='text-blue-900 text-2xl'/>
+   < RiTailwindCssFill className="text-blue-900 text-2xl"/>
      </div>
      <h2 className="text-white  font-semibold tracking-tight">
        T-BLOCK
        </h2>
    </div>
      <div className="w-full h-1 rounded bg-black">
-       <div className='w-[88%] h-1 bg-white rounded-lg'/>
+       <div className="w-[88%] h-1 bg-white rounded-lg"/>
      </div>
-     <h3 className='text-white text-sm font-semibold text-right tracking-tight'>88%</h3>
+     <h3 className="text-white text-sm font-semibold text-right tracking-tight">88%</h3>
  </div>
 
 
@@ -152,16 +152,16 @@ function MobileSkills() {
  <div className="p-4 w-full md:w-1/3 ">
    <div className="flex items-center mb-3">
      <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-white text-white flex-shrink-0">
-   < FaFigma className='text-blue-900 text-lg'/>
+   < FaFigma className="text-blue-900 text-lg"/>
      </div>
      <h2 className="text-white  font-semibold tracking-tight">
        FIGMA(UI/UX)
        </h2>
    </div>
      <div className="w-full h-1 rounded bg-black">
-       <div className='w-[75%] h-1 bg-white rounded-lg'/>
+       <div className="w-[75%] h-1 bg-white rounded-lg"/>
      </div>
-     <h3 className='text-white text-sm font-semibold text-right tracking-tight'>75%</h3>
+     <h3 className="text-white text-sm font-semibold text-right tracking-tight">75%</h3>
  </div>
 
 
@@ -169,7 +169,7 @@ function MobileSkills() {
  <div className="p-4 w-full md:w-1/3 ">
    <div className="flex items-center mb-3">
      <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-white text-white flex-shrink-0">
-   < FaBootstrap className='text-blue-900 text-xl'/>
+   < FaBootstrap className="text-blue-900 text-xl"/>
      </div>
      <h2 className="text-white text-lg font-semibold tracking-tight">
        BOOTSTRAP
@@ -186,16 +186,16 @@ function MobileSkills() {
  <div className="p-4 w-full md:w-1/3 ">
    <div className="flex items-center mb-3">
      <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-white text-white flex-shrink-0">
-   < SiShadcnui className='text-blue-900 text-lg'/>
+   < SiShadcnui className="text-blue-900 text-lg"/>
      </div>
      <h2 className="text-white font-semibold tracking-tight">
        SHADCN 
        </h2>
    </div>
      <div className="w-full h-1 rounded bg-black">
-       <div className='w-[90%] h-1 bg-white rounded-lg'/>
+       <div className="w-[90%] h-1 bg-white rounded-lg"/>
      </div>
-     <h3 className='text-white text-sm font-semibold text-right tracking-tight'>90%</h3>
+     <h3 className="text-white text-sm font-semibold text-right tracking-tight">90%</h3>
  </div>
 
 
@@ -203,16 +203,16 @@ function MobileSkills() {
  <div className="p-4 w-full md:w-1/3 ">
    <div className="flex items-center mb-3">
      <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-white text-white flex-shrink-0">
-   < FaNpm className='text-blue-900 text-3xl'/>
+   < FaNpm className="text-blue-900 text-3xl"/>
      </div>
      <h2 className="text-white text-lg font-semibold tracking-tight">
        NPM
        </h2>
    </div>
      <div className="w-full h-1 rounded bg-black">
-       <div className='w-[93%] h-1 bg-white rounded-lg'/>
+       <div className="w-[93%] h-1 bg-white rounded-lg"/>
      </div>
-     <h3 className='text-white text-sm font-semibold text-right tracking-tight'>93%</h3>
+     <h3 className="text-white text-sm font-semibold text-right tracking-tight">93%</h3>
  </div>
 
 
@@ -220,16 +220,16 @@ function MobileSkills() {
  <div className="p-4 w-full md:w-1/3 ">
    <div className="flex items-center mb-3">
      <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-white text-white flex-shrink-0">
-   < FaPython className='text-blue-900 text-2xl'/>
+   < FaPython className="text-blue-900 text-2xl"/>
      </div>
      <h2 className="text-white text-lg font-semibold tracking-tight">
        PYTHON
        </h2>
    </div>
      <div className="w-full h-1 rounded bg-black">
-       <div className='w-[90%] h-1 bg-white rounded-lg'/>
+       <div className="w-[90%] h-1 bg-white rounded-lg"/>
      </div>
-     <h3 className='text-white text-sm font-semibold text-right tracking-tight'>90%</h3>
+     <h3 className="text-white text-sm font-semibold text-right tracking-tight">90%</h3>
  </div>
 
 
@@ -237,16 +237,16 @@ function MobileSkills() {
  <div className="p-4 w-full md:w-1/3 ">
    <div className="flex items-center mb-3">
      <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-white text-white flex-shrink-0">
-   < AiOutlineOpenAI className='text-blue-900 text-3xl'/>
+   < AiOutlineOpenAI className="text-blue-900 text-3xl"/>
      </div>
      <h2 className="text-white text-lg font-semibold tracking-tight">
        Prompt.Eng (GPT-3/4)
        </h2>
    </div>
      <div className="w-full h-1 rounded bg-black">
-       <div className='w-[94%] h-1 bg-white rounded-lg'/>
+       <div className="w-[94%] h-1 bg-white rounded-lg"/>
      </div>
-     <h3 className='text-white text-sm font-semibold text-right tracking-tight'>94%</h3>
+     <h3 className="text-white text-sm font-semibold text-right tracking-tight">94%</h3>
  </div>
 
 
@@ -254,16 +254,16 @@ function MobileSkills() {
  <div className="p-4 w-full md:w-1/3 ">
    <div className="flex items-center mb-3">
      <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-white text-white flex-shrink-0">
-   < SiGooglecolab className='text-blue-900 text-2xl'/>
+   < SiGooglecolab className="text-blue-900 text-2xl"/>
      </div>
      <h2 className="text-white text-lg font-semibold tracking-tight">
        G_COLAB
        </h2>
    </div>
      <div className="w-full h-1 rounded bg-black">
-       <div className='w-[70%] h-1 bg-white rounded-lg'/>
+       <div className="w-[70%] h-1 bg-white rounded-lg"/>
      </div>
-     <h3 className='text-white text-sm font-semibold text-right tracking-tight'>70%</h3>
+     <h3 className="text-white text-sm font-semibold text-right tracking-tight">70%</h3>
  </div>
 
 
@@ -271,16 +271,16 @@ function MobileSkills() {
  <div className="p-4 w-full md:w-1/3 ">
    <div className="flex items-center mb-3">
      <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-white text-white flex-shrink-0">
-   < SiJupyter className='text-blue-900 text-2xl'/>
+   < SiJupyter className="text-blue-900 text-2xl"/>
      </div>
      <h2 className="text-white text-lg font-semibold tracking-tight">
        JUPYTER NOTEBOOK
        </h2>
    </div>
      <div className="w-full h-1 rounded bg-black">
-       <div className='w-[60%] h-1 bg-white rounded-lg'/>
+       <div className="w-[60%] h-1 bg-white rounded-lg"/>
      </div>
-     <h3 className='text-white text-sm font-semibold text-right tracking-tight'>60%</h3>
+     <h3 className="text-white text-sm font-semibold text-right tracking-tight">60%</h3>
  </div>
 
 
@@ -288,16 +288,16 @@ function MobileSkills() {
  <div className="p-4 w-full md:w-1/3 ">
    <div className="flex items-center mb-3">
      <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-white text-white flex-shrink-0">
-   < FaDocker className='text-blue-900 text-2xl'/>
+   < FaDocker className="text-blue-900 text-2xl"/>
      </div>
      <h2 className="text-white text-lg font-semibold tracking-tight">
        DOCKER
        </h2>
    </div>
      <div className="w-full h-1 rounded bg-black">
-       <div className='w-[65%] h-1 bg-white rounded-lg'/>
+       <div className="w-[65%] h-1 bg-white rounded-lg"/>
      </div>
-     <h3 className='text-white text-sm font-semibold text-right tracking-tight'>65%</h3>
+     <h3 className="text-white text-sm font-semibold text-right tracking-tight">65%</h3>
  </div>
 
 
@@ -305,7 +305,7 @@ function MobileSkills() {
  <div className="p-4 w-full md:w-1/3 ">
    <div className="flex items-center mb-3">
      <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-white text-white flex-shrink-0">
-   < FaLinux className='text-blue-900 text-3xl'/>
+   < FaLinux className="text-blue-900 text-3xl"/>
      </div>
      <h2 className="text-white text-lg font-semibold tracking-tight">
        UBUNTU
@@ -314,7 +314,7 @@ function MobileSkills() {
      <div className="w-full h-1 rounded bg-black">
        <div className='w-[50%] h-1 bg-white rounded-lg'/>
      </div>
-     <h3 className='text-white text-sm font-semibold text-right tracking-tight'>50%</h3>
+     <h3 className="text-white text-sm font-semibold text-right tracking-tight">50%</h3>
  </div>
 
 
@@ -322,16 +322,16 @@ function MobileSkills() {
  <div className="p-4 w-full md:w-1/3 ">
    <div className="flex items-center mb-3">
      <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-white text-white flex-shrink-0">
-   < FaGithub className='text-blue-900 text-2xl'/>
+   < FaGithub className="text-blue-900 text-2xl"/>
      </div>
      <h2 className="text-white text-lg font-semibold tracking-tight">
        GIT/GITHUB
        </h2>
    </div>
      <div className="w-full h-1 rounded bg-black">
-       <div className='w-[91%] h-1 bg-white rounded-lg'/>
+       <div className="w-[91%] h-1 bg-white rounded-lg"/>
      </div>
-     <h3 className='text-white text-sm font-semibold text-right tracking-tight'>91%</h3>
+     <h3 className="text-white text-sm font-semibold text-right tracking-tight">91%</h3>
  </div>
 
 
@@ -339,16 +339,16 @@ function MobileSkills() {
  <div className="p-4 w-full md:w-1/3 ">
    <div className="flex items-center mb-3">
      <div className="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-white text-white flex-shrink-0">
-   <FaGithub  className='text-blue-900 text-2xl'/>
+   <FaGithub  className="text-blue-900 text-2xl"/>
      </div>
      <h2 className="text-white text-lg font-semibold tracking-tight">
        GITHUB DESKTOP
        </h2>
    </div>
      <div className="w-full h-1 rounded bg-black">
-       <div className='w-[85%] h-1 bg-white rounded-lg'/>
+       <div className="w-[85%] h-1 bg-white rounded-lg"/>
      </div>
-     <h3 className='text-white text-sm font-semibold text-right tracking-tight'>85%</h3>
+     <h3 className="text-white text-sm font-semibold text-right tracking-tight">85%</h3>
  </div>
 
 
